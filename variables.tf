a420c0f538061c5680f179a78ffdc0db1b3bc304
sam dino <smdino08@outlook.com>
2026-09-19 16:23:14 -0400

    update variables file

--- variables.tf
+++ variables.tf
@@ -13,22 +13,25 @@ variable "instance_type" {
 variable "ami_id" {
   description = "AMI ID to use. Defaults to the latest Amazon Linux 2 AMI if left null."
   type        = string
-  default     = ami-0aeec3e3fd048bf12
+  default     = "ami-0aeec3e3fd048bf12"
 }
 
 variable "key_name" {
   description = "Name of an existing EC2 key pair for SSH access"
-  type        = windows
+  type        = string
+  default = "windows"
 }
 
 variable "vpc_id" {
   description = "VPC ID where the security group will be created"
-  type        = vpc-06f412ee7552bbc3a
+  type        = string
+  default = "vpc-06f412ee7552bbc3a"
 }
 
 variable "subnet_id" {
   description = "Subnet ID where the instance will be launched"
-  type        = subnet-03fccbf9124959c5a
+  type        = string
+  default = "subnet-03fccbf9124959c5a"
 }
 
 variable "allowed_ssh_cidr_blocks" {
