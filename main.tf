2ec215e920b6c30978916dbd8c9e354d2dfee26c
samdino08 <smdino08@outlook.com>
2026-09-19 17:55:38 -0400

    Fix user_data template file extension from .tp to .tpl

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_instance" "jenkins" {
     volume_type = "gp3"
   }
 
-  user_data = templatefile("${path.module}/user_data.sh.tp", {})
+  user_data = templatefile("${path.module}/user_data.sh.tpl", {})
 
   tags = merge(
     var.tags,
